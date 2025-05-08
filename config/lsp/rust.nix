1b725cafd81f0ffde54bc86d0198b52da27243f3
Maarten Behn <maarten.behn@gmail.com>
2025-05-08 11:44:05 +0200

    rust features

--- config/lsp/rust.nix
+++ config/lsp/rust.nix
@@ -16,6 +16,9 @@
         };
         tools.enable_clippy = false;
         server = {
+          #on_attach.__raw = ''function(client, bufnr) 
+          #  vim.cmd.RustAnalyzer { 'config', '{ server.rust-analyzer.cargo.features = { "tree64" } }' }
+          #end'';
           default_settings = {
             inlayHints = {
               lifetimeElisionHints = {
@@ -24,10 +27,46 @@
             };
             rust-analyzer = {
               diagnostics.enable = false;
+              #cargo.features = [ "tree64" ];
             };
           };
         };
       };
     };
   };
+
+  extraConfigLua = ''
+    -- Load Features from env var RUST_FEATURES
+    local project_features_env = os.getenv("RUST_FEATURES")
+    local project_features = {}
+
+    if project_features_env ~= nil and project_features_env ~= "" then
+      local function split(str, sep)
+        local result = {}
+        local regex = ("([^%s]+)"):format(sep)
+        for each in str:gmatch(regex) do
+          table.insert(result, each)
+        end
+        return result
+      end
+      project_features = split(project_features_env, ",")
+    end
+
+    vim.g.rustaceanvim = {
+      -- LSP configuration
+      server = {
+        default_settings = {
+          -- rust-analyzer language server configuration
+          ['rust-analyzer'] = {
+            cargo = {
+              features = project_features,
+            },
+            check = {
+              features = project_features,
+            },
+          },
+        },
+      },
+    }
+  '';
 }

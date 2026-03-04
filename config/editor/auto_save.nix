5a471867fb33cb9cb8f5ab8a999203bb690784ce
Maarten Behn <maarten.behn@gmail.com>
2026-03-04 11:02:13 +0100

    stuff

--- config/editor/auto_save.nix
+++ config/editor/auto_save.nix
@@ -1,5 +1,19 @@
 {
   plugins.auto-save = {
     enable = true;
+    settings = {
+      write_all_buffers = true;
+    };
   };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>ma";
+      action = "<cmd>ASToggle<CR>";
+      options = {
+        desc = "Toggle Auto Save";
+      };
+    }
+  ];
 }
